d41c9a8a4c90d5bab02717dc27d8f4ecbb3d54ab
Bilgehan BERBEROGLU <Bilgehan.BERBEROGLU@sodexo.com>
2019-11-19 09:53:10 +0300

    dotnet core framework version updated to 3

--- UnitOfWork.Host/Startup.cs
+++ UnitOfWork.Host/Startup.cs
@@ -37,15 +37,19 @@ namespace Arch.EntityFrameworkCore.UnitOfWork.Host
                 .AddUnitOfWork<BloggingContext>()
                 .AddCustomRepository<Blog, CustomBlogRepository>();
 
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddConfiguration(Configuration.GetSection("Logging"));
+                loggingBuilder.AddConsole();
+                loggingBuilder.AddDebug();
+            });
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             app.UseMvc();
         }
     }
